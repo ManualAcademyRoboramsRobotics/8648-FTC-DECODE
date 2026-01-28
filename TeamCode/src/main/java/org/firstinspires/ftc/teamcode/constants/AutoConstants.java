ba2708b2567e5c74ba8c6016c39e220fa5cf6921
Isaac Glenzinski <isaacglenzinski@gmail.com>
2026-01-27 18:04:36 -0600

    Cleanup and final tuning before meet

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/AutoConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/AutoConstants.java
@@ -6,8 +6,23 @@ import com.acmerobotics.dashboard.config.Config;
 public class AutoConstants {
     //http://192.168.43.1:8080/dash
     public static long DIVERTER_DELAY = 750;
-    public static double RED_SPIKE_X = 28;
-    public static double BLUE_SPIKE_X = -28;
-    public static double ARTIFACT_LENGTH_IN = 5.00;
+    public static double ARTIFACT_LENGTH_IN = 6.00;
+    public static double CLOSE_START_X = 40.00;
+    public static double CLOSE_START_Y = 60.00;
+    public static double CLOSE_START_H = 0.00;
+    public static double FAR_START_X = 24.00;
+    public static double FAR_START_Y = -62.00;
+    public static double FAR_START_H = 0.00;
+    public static double CLOSE_LAUNCH_X = 20.00;
+    public static double CLOSE_LAUNCH_Y = 20.00;
+    public static double CLOSE_LAUNCH_H = 45.00;
+    public static double FAR_LAUNCH_X = 14.50;
+    public static double FAR_LAUNCH_Y = -50.00;
+    public static double FAR_LAUNCH_H = 24.80;
+    public static double SPIKE_X = 31.00;
+    public static double SPIKE_1_Y = 10.00;
+    public static double SPIKE_2_Y = -10.00; //-10.00;
+    public static double SPIKE_3_Y = -35.00; //-34.00;
+
 
 }
